4305fa48aa9f1590044b62df26a1ea21511fe6e9
ankitgwari <ankitgwari14@gmail.com>
2024-07-07 21:06:34 +0530

    delivery and price calculation complete

---
+++ scripts/checkout.js
@@ -0,0 +1,6 @@
+import { renderHeader } from "./checkout/header.js";
+import { renderOrderSummary } from "./checkout/orderSummary.js";
+import {renderPaymentSummary} from "./checkout/paymentSummary.js";
+renderHeader();
+renderOrderSummary();
+renderPaymentSummary();
