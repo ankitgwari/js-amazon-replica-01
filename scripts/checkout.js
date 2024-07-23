fe7f1b772d51b04ed250c96162b9454682d320db
ankitgwari <ankitgwari14@gmail.com>
2024-07-23 12:37:54 +0530

    convert promise to async await function

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -4,7 +4,26 @@ import { renderHeader } from "./checkout/header.js";
 import { renderOrderSummary } from "./checkout/orderSummary.js";
 import { renderPaymentSummary } from "./checkout/paymentSummary.js";
 
+//async await function
+async function loadPage(){
+
+    await loadProductsFetch();
+
+    await new Promise((resolve)=>{
+        loadCart(()=>{
+            resolve();
+        })
+    });
+
+    renderHeader();
+    renderOrderSummary();
+    renderPaymentSummary();
+
+}
+loadPage();
+
 //running all promise together
+/*
 Promise.all([
     loadProductsFetch(),
     new Promise((resolve)=>{
@@ -17,27 +36,30 @@ Promise.all([
     renderOrderSummary();
     renderPaymentSummary();
 });
-
+*/
 //promise instead of callback;
-// new Promise((resolve)=>{
-//     loadProducts(()=>{
-//         resolve();
-//     })
-// }).then (()=>{
-//     //next step
-//     return new Promise((resolve)=>{
-//         loadCart(()=>{
-//             resolve();
-//         })
-//     });
-// }).then(()=>{
-//     renderHeader();
-//     renderOrderSummary();
-//     renderPaymentSummary();
-// })
-
-// loadProducts(() => {
-//     renderHeader();
-//     renderOrderSummary();
-//     renderPaymentSummary();
-// });
+/*
+new Promise((resolve)=>{
+    loadProducts(()=>{
+        resolve();
+    })
+}).then (()=>{
+    //next step
+    return new Promise((resolve)=>{
+        loadCart(()=>{
+            resolve();
+        })
+    });
+}).then(()=>{
+    renderHeader();
+    renderOrderSummary();
+    renderPaymentSummary();
+})
+*/
+/*
+loadProducts(() => {
+    renderHeader();
+    renderOrderSummary();
+    renderPaymentSummary();
+});
+*/
